f7697e37420395c66cf4fe89c5574be013bd57f5
Vladyslav Mykhailenko1 <Vladyslav_Mykhailenko1@epam.com>
2017-03-09 16:32:04 +0200

    Updated Coin enum
    added int value field
    added constructor, getValue

--- src/main/java/app/model/Coin.java
+++ src/main/java/app/model/Coin.java
@@ -4,9 +4,19 @@ package app.model;
  * This enum represents values of coins accepted by the vending machine
  */
 public enum Coin {
-    One,
-    Five,
-    Ten,
-    TwntyFive,
-    Fifty
+    One(1),
+    Five(5),
+    Ten(10),
+    TwntyFive(25),
+    Fifty(50);
+
+    private int value;
+
+    Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
